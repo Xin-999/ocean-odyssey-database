8ff7d29ef8a9b0a6066eacfeb9583bf4abb7e31b
AngQiaoXin <qang0004@student.monash.edu>
2025-06-01 20:18:07 +0800

    Created table competitor following the same attribute name and meaning as the comment from pdf given.

--- Assignments/Ass2/T1-rm-schema.sql
+++ Assignments/Ass2/T1-rm-schema.sql
@@ -26,7 +26,31 @@ DROP TABLE team CASCADE CONSTRAINTS PURGE;
 -- FK constraints are to be added at the end of this script
 
 -- COMPETITOR
+CREATE TABLE competitor (
+    comp_no         NUMBER(5)     NOT NULL,
+    comp_fname      VARCHAR2(30)  NOT NULL,
+    comp_lname      VARCHAR2(30)  NOT NULL,
+    comp_gender     CHAR(1)       NOT NULL,
+    comp_dob        DATE          NOT NULL,
+    comp_email      VARCHAR2(50)  NOT NULL,
+    comp_unistatus  CHAR(1)       NOT NULL,
+    comp_phone      CHAR(10)      NOT NULL
+);
 
+COMMENT ON COLUMN competitor.comp_no IS 'Unique identifier for a competitor';
+COMMENT ON COLUMN competitor.comp_fname IS 'Competitor''s first name';
+COMMENT ON COLUMN competitor.comp_lname IS 'Competitor''s last name';
+COMMENT ON COLUMN competitor.comp_gender IS 'Competitor''s gender (''M'' for male, ''F'' for female, or ''U'' for ''Undisclosed'')';
+COMMENT ON COLUMN competitor.comp_dob IS 'Competitor''s date of birth';
+COMMENT ON COLUMN competitor.comp_email IS 'Competitor''s email - unique for each competitor';
+COMMENT ON COLUMN competitor.comp_unistatus IS 'Competitor is a university student or staff (''Y'' for Yes or ''N'' for No)';
+COMMENT ON COLUMN competitor.comp_phone IS 'Competitor''s phone number - unique for each competitor';
+
+ALTER TABLE competitor ADD CONSTRAINT competitor_pk PRIMARY KEY (comp_no);
+ALTER TABLE competitor ADD CONSTRAINT competitor_uq1 UNIQUE (comp_email);
+ALTER TABLE competitor ADD CONSTRAINT competitor_uq2 UNIQUE (comp_phone);
+ALTER TABLE competitor ADD CONSTRAINT chk_comp_gender CHECK (comp_gender IN ('M', 'F', 'U'));
+ALTER TABLE competitor ADD CONSTRAINT chk_comp_unistatus CHECK (comp_unistatus IN ('Y', 'N'));
 
 
 --ENTRY
