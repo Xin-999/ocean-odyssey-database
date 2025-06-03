2d11b8b81a92b60605aca9100d6d68fe18ab1df5
AngQiaoXin <qang0004@student.monash.edu>
2025-06-03 20:17:48 +0800

    Added changes to database about part c and d. make it by deleting and inserting again instead of update becasue the entry_no might be different in different event.

--- Assignments/Ass2/T3-rm-dm.sql
+++ Assignments/Ass2/T3-rm-dm.sql
@@ -81,7 +81,76 @@ VALUES (
 
 --(c)
 
---(d)
+-- 1. DELETE Jackson's existing 10K entry
+DELETE FROM entry
+WHERE comp_no = (
+    SELECT comp_no FROM competitor WHERE comp_phone = '0422412524'
+)
+AND event_id = (
+    SELECT event_id FROM event 
+    WHERE carn_date = (
+        SELECT carn_date FROM carnival 
+        WHERE UPPER(carn_name) = UPPER('RM WINTER SERIES CAULFIELD 2025')
+    )
+    AND UPPER(eventtype_code) = UPPER('10K')
+);
 
+-- 2. INSERT new 5K entry for Jackson with updated charity
+INSERT INTO entry (
+    event_id, entry_no, entry_starttime, entry_finishtime, entry_elapsedtime,
+    comp_no, team_id, char_id
+)
+VALUES (
+    -- event_id for 5K in same carnival
+    (SELECT event_id FROM event 
+     WHERE carn_date = (
+         SELECT carn_date FROM carnival 
+         WHERE UPPER(carn_name) = UPPER('RM WINTER SERIES CAULFIELD 2025')
+     )
+     AND UPPER(eventtype_code) = UPPER('5K')),
+
+    -- next available entry_no
+    (SELECT NVL(MAX(entry_no), 0) + 1 FROM entry 
+     WHERE event_id = (
+         SELECT event_id FROM event 
+         WHERE carn_date = (
+             SELECT carn_date FROM carnival 
+             WHERE UPPER(carn_name) = UPPER('RM WINTER SERIES CAULFIELD 2025')
+         )
+         AND UPPER(eventtype_code) = UPPER('5K'))),
+
+    NULL,NULL,NULL,
+
+    -- comp_no for Jackson
+    (SELECT comp_no FROM competitor WHERE comp_phone = '0422412524'),
+
+    -- assuming no team now
+    NULL,
+
+    -- new charity: Beyond Blue
+    (SELECT char_id FROM charity WHERE UPPER(char_name) = UPPER('Beyond Blue'))
+);
+
+--(d)
+DELETE FROM entry
+WHERE comp_no = (
+    SELECT comp_no FROM competitor
+    WHERE comp_phone = '0422141112'
+)
+AND event_id = (
+    SELECT event_id FROM event
+    WHERE carn_date = (
+        SELECT carn_date FROM carnival
+        WHERE UPPER(carn_name) = UPPER('RM WINTER SERIES CAULFIELD 2025')
+    )
+    AND UPPER(eventtype_code) = UPPER('10K')
+);
+
+DELETE FROM team
+WHERE UPPER(team_name) = UPPER('Super Runners')
+AND carn_date = (
+    SELECT carn_date FROM carnival
+    WHERE UPPER(carn_name) = UPPER('RM WINTER SERIES CAULFIELD 2025')
+);
 
 COMMIT;
