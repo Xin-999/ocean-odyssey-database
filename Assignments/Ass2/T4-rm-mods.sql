98ff2f83cf1c31b0e81753c87a3e5d09d0631b6c
AngQiaoXin <qang0004@student.monash.edu>
2025-06-03 20:33:06 +0800

    Added select statement to show part 4a. Created new table for part b, to store percentage of charity of the competitors.

--- Assignments/Ass2/T4-rm-mods.sql
+++ Assignments/Ass2/T4-rm-mods.sql
@@ -23,9 +23,24 @@ SET completed_events = (
       AND e.entry_finishtime IS NOT NULL
 );
 
+SELECT comp_no, comp_fname, comp_lname, completed_events
+FROM competitor
+ORDER BY comp_no;
 DESC competitor;
 
-
 --(b)
+-- Create the associative table
+CREATE TABLE charity_support (
+    comp_no     NUMBER(4) NOT NULL,
+    char_id     NUMBER(3) NOT NULL,
+    carn_date   DATE NOT NULL,
+    percentage  NUMBER(3) CHECK (percentage BETWEEN 0 AND 100),
+
+    CONSTRAINT charity_support_pk PRIMARY KEY (comp_no, char_id, carn_date),
+    CONSTRAINT comp_fk FOREIGN KEY (comp_no) REFERENCES competitor (comp_no),
+    CONSTRAINT char_fk FOREIGN KEY (char_id) REFERENCES charity (char_id),
+    CONSTRAINT carn_fk FOREIGN KEY (carn_date) REFERENCES carnival (carn_date)
+);
+
 
 
