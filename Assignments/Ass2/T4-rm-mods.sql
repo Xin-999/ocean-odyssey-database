cee2c7b4ace36f91348e184502a5e17a7e828e24
AngQiaoXin <qang0004@student.monash.edu>
2025-06-04 00:01:52 +0800

    Added insert jackson bull charity support percentages.

--- Assignments/Ass2/T4-rm-mods.sql
+++ Assignments/Ass2/T4-rm-mods.sql
@@ -30,11 +30,13 @@ DESC competitor;
 
 --(b)
 -- Create the associative table
+
+DROP TABLE charity_support CASCADE CONSTRAINTS PURGE;
 CREATE TABLE charity_support (
     comp_no     NUMBER(4) NOT NULL,
     char_id     NUMBER(3) NOT NULL,
     carn_date   DATE NOT NULL,
-    percentage  NUMBER(3) CHECK (percentage BETWEEN 0 AND 100),
+    percentage  NUMBER(3) NOT NULL CHECK (percentage BETWEEN 0 AND 100),
 
     CONSTRAINT charity_support_pk PRIMARY KEY (comp_no, char_id, carn_date),
     CONSTRAINT comp_fk FOREIGN KEY (comp_no) REFERENCES competitor (comp_no),
@@ -42,5 +44,26 @@ CREATE TABLE charity_support (
     CONSTRAINT carn_fk FOREIGN KEY (carn_date) REFERENCES carnival (carn_date)
 );
 
+-- Insert Jackson Bull's charity support percentages
+INSERT INTO charity_support (comp_no, char_id, carn_date, percentage)
+VALUES (
+  (SELECT comp_no FROM competitor WHERE UPPER(comp_fname) = UPPER('Jackson') AND UPPER(comp_lname) = UPPER('Bull')),
+  (SELECT char_id FROM charity WHERE UPPER(char_name) = UPPER('RSPCA')),
+  (SELECT carn_date FROM carnival WHERE UPPER(carn_name) = UPPER('RM WINTER SERIES CAULFIELD 2025')),
+  70
+);
 
+INSERT INTO charity_support (comp_no, char_id, carn_date, percentage)
+VALUES (
+  (SELECT comp_no FROM competitor WHERE UPPER(comp_fname) = UPPER('Jackson') AND UPPER(comp_lname) = UPPER('Bull')),
+  (SELECT char_id FROM charity WHERE UPPER(char_name) = UPPER('Beyond Blue')),
+  (SELECT carn_date FROM carnival WHERE UPPER(carn_name) = UPPER('RM WINTER SERIES CAULFIELD 2025')),
+  30
+);
 
+SELECT * FROM charity_support
+ORDER BY comp_no, carn_date, char_id;
+
+DESC charity_support;
+
+COMMIT;
