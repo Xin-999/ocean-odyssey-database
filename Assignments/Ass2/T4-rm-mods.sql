623ebe745e6c9b0ef25ec086e136865a140b2464
AngQiaoXin <qang0004@student.monash.edu>
2025-06-03 20:19:42 +0800

    Added new column to competitor about showing completed event of that participant.

--- Assignments/Ass2/T4-rm-mods.sql
+++ Assignments/Ass2/T4-rm-mods.sql
@@ -1,8 +1,8 @@
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --T4-rm-mods.sql
 
---Student ID:
---Student Name:
+--Student ID: 33520054
+--Student Name: Ang Qiao Xin
 
 /* Comments for your marker:
 
@@ -12,6 +12,18 @@
 */
 
 --(a)
+-- Add the new column
+ALTER TABLE competitor ADD completed_events NUMBER(3);
+
+UPDATE competitor c
+SET completed_events = (
+    SELECT COUNT(*) FROM entry e
+    WHERE e.comp_no = c.comp_no
+      AND e.entry_starttime IS NOT NULL
+      AND e.entry_finishtime IS NOT NULL
+);
+
+DESC competitor;
 
 
 --(b)
